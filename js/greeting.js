7dcdb34cc8b0ef7d36e382d8c61808277fb65c11
wotikama <111455424+wotikama@users.noreply.github.com>
2024-03-15 13:44:38 +0100

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,7 +3,7 @@ let today = new Date();
 let hour = today.getHours();
 
 // Here you can change your name
-let user = 'Tuur';
+let user = 'Pablo';
 
 // Here you can change your greetings
 let gree1 = 'Good night, ';
@@ -12,7 +12,7 @@ let gree3 = 'Good afternoon, ';
 let gree4 = 'Good evening, ';
 
 // Define the hours of the greetings
-if (hour >= 23 && hour < 5) {
+if (hour >= 21 && hour < 5) {
     document.querySelector('.c-greeting').innerText = gree1 + user;
 } else if (hour >= 6 && hour < 12) {
     document.querySelector('.c-greeting').innerText = gree2 + user;
